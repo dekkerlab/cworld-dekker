7557bbe873e623e9059482722922faca4e784ad0
sergpolly <sergpolly@gmail.com>
2019-06-07 12:37:37 -0400

    almost running docker

---
+++ docker/build.sh
@@ -0,0 +1,16 @@
+set -ex
+
+USERNAME=dekkerlab
+IMAGE=cworld_env
+
+cp  ../VERSION ./VERSION
+cp ../cworld_environment.yml ./cworld_environment.yml
+
+function cleanup {
+    rm  ./VERSION
+    rm  ./cworld_environment.yml
+}
+
+trap cleanup EXIT
+
+docker build -t $USERNAME/$IMAGE:latest . 
